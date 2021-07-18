182f75d7a6c99498143b067370ed2137e3c159bd
Nitin <nk17oct@gmail.com>
2021-07-18 18:10:08 +0530

    remove appbar from user dashboard

--- src/Components/userDashboard/Dashboard.jsx
+++ src/Components/userDashboard/Dashboard.jsx
@@ -4,28 +4,21 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MainDashboard from './MainDashboard';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SubjectIcon from '@material-ui/icons/Subject';
 import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import MainDashboard from './MainDashboard';
 
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,21 +34,6 @@ const useStyles = makeStyles((theme) => ({
     padding: '0 8px',
     ...theme.mixins.toolbar,
   },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
   menuButton: {
     marginRight: 36,
   },
@@ -68,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
   drawerPaper: {
     position: 'relative',
     whiteSpace: 'nowrap',
-    width: drawerWidth,
+    width: '240px',
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
@@ -104,6 +82,13 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     height: 240,
   },
+  drawerListText:{
+    color: 'initial',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'none'
+    }
+  },
 }));
 
 export default function Dashboard() {
@@ -122,27 +107,6 @@ export default function Dashboard() {
     <BrowserRouter>
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Dashboard
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
       <Drawer
         variant="permanent"
         classes={{
@@ -151,14 +115,14 @@ export default function Dashboard() {
         open={open}
       >
         <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
+          <IconButton onClick={open?handleDrawerClose:handleDrawerOpen}>
             <ChevronLeftIcon />
           </IconButton>
         </div>
         <Divider />
 
         <List>
-          <Link to={`${url}`}>
+          <Link to={`${url}`} className={classes.drawerListText}>
             <ListItem button>
               <ListItemIcon>
                 <DashboardIcon />
@@ -167,16 +131,16 @@ export default function Dashboard() {
             </ListItem>
           </Link>
 
-          <Link to={`${url}/research-statement`}>
+          <Link to={`${url}/research-statement`} className={classes.drawerListText}>
             <ListItem button>
               <ListItemIcon>
-                <ShoppingCartIcon />
+                <SubjectIcon />
               </ListItemIcon>
               <ListItemText primary="Research statement" />
             </ListItem>
           </Link>
 
-          <Link to={`${url}/announcements`}>
+          <Link to={`${url}/announcements`} className={classes.drawerListText}>
             <ListItem button>
               <ListItemIcon>
                 <LayersIcon />
@@ -185,7 +149,7 @@ export default function Dashboard() {
             </ListItem>
           </Link>
 
-          <Link to={`${url}/new-applications`}>
+          <Link to={`${url}/new-applications`} className={classes.drawerListText}>
             <ListItem button>
               <ListItemIcon>
                 <BarChartIcon />
@@ -194,7 +158,7 @@ export default function Dashboard() {
             </ListItem>
           </Link>
 
-          <Link to={`${url}/profile`}>
+          <Link to={`${url}/profile`} className={classes.drawerListText}>
             <ListItem button>
               <ListItemIcon>
                 <PeopleIcon />
@@ -203,7 +167,7 @@ export default function Dashboard() {
             </ListItem>
           </Link>
 
-          <Link to={`${url}/resume`}>
+          <Link to={`${url}/resume`} className={classes.drawerListText}>
             <ListItem button>
               <ListItemIcon>
                 <AssignmentIcon />

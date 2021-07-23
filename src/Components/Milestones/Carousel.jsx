cab0249f805173b35d5dcfbeabed06e4a44d7589
Moinak Banerjee <32922277+moinak878@users.noreply.github.com>
2021-07-23 08:50:09 +0530

    carousel linting error resolved (hopefully...)

--- src/Components/Milestones/Carousel.jsx
+++ src/Components/Milestones/Carousel.jsx
@@ -1,137 +1,137 @@
-import React, { useState, useEffect } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { Grid, Slide, makeStyles } from "@material-ui/core";
-import PropTypes from "prop-types";
-import CarouselSlide from "./CarouselSlider";
+import React, { useState, useEffect } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { Grid, Slide, makeStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import CarouselSlide from './CarouselSlider';
 
 const useStyles = makeStyles(() => ({
-	arrows: {
-		height: "30px",
-		margin: "0",
-		cursor: "pointer",
-		position: "relative",
-		transform: "translateY(25vh)",
-	},
-	slideshowDots: {
-		textAlign: "center",
-		justifyContent: "center",
-	},
-	slideshowDot: {
-		display: "block",
-		height: "10px",
-		maxWidth: "10px",
-		margin: "26px 7px 0px",
-		borderRadius: "50%",
-		cursor: "pointer",
-		backgroundColor: "#636363",
-	},
-	slideshowDotActive: {
-		display: "block",
-		height: "10px",
-		maxWidth: "10px",
-		margin: "26px 7px 0px",
-		borderRadius: "50%",
-		cursor: "pointer",
-		transform: "scale(1.2)",
-		backgroundColor: "white",
-		border: "1px solid #636363",
-	},
+  arrows: {
+    height: '30px',
+    margin: '0',
+    cursor: 'pointer',
+    position: 'relative',
+    transform: 'translateY(25vh)',
+  },
+  slideshowDots: {
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  slideshowDot: {
+    display: 'block',
+    height: '10px',
+    maxWidth: '10px',
+    margin: '26px 7px 0px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    backgroundColor: '#636363',
+  },
+  slideshowDotActive: {
+    display: 'block',
+    height: '10px',
+    maxWidth: '10px',
+    margin: '26px 7px 0px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    transform: 'scale(1.2)',
+    backgroundColor: 'white',
+    border: '1px solid #636363',
+  },
 }));
 
-const pic1 = require("../../Assets/chicago.jpg");
-const pic2 = require("../../Assets/ny.jpg");
-const pic3 = require("../../Assets/la.jpg");
+const pic1 = require('../../Assets/chicago.jpg');
+const pic2 = require('../../Assets/ny.jpg');
+const pic3 = require('../../Assets/la.jpg');
 
 const SLIDE_INFO = [
-	{ id: 1, title: "chicago", image: pic1 },
-	{ id: 2, title: "la", image: pic2 },
-	{ id: 3, title: "ny", image: pic3 },
+  { id: 1, title: 'chicago', image: pic1 },
+  { id: 2, title: 'la', image: pic2 },
+  { id: 3, title: 'ny', image: pic3 },
 ];
 
 function Arrow(props) {
-	Arrow.propTypes = {
-		direction: PropTypes.string,
-		clickFunction: PropTypes.func,
-	};
-	Arrow.defaultProps = {
-		direction: "left",
-		clickFunction: () => {
-			// do nothing
-		},
-	};
-	const { direction, clickFunction } = props;
-	const icon = direction === "left" ? <FaChevronLeft /> : <FaChevronRight />;
+  Arrow.propTypes = {
+    direction: PropTypes.string,
+    clickFunction: PropTypes.func,
+  };
+  Arrow.defaultProps = {
+    direction: 'left',
+    clickFunction: () => {
+      // do nothing
+    },
+  };
+  const { direction, clickFunction } = props;
+  const icon = direction === 'left' ? <FaChevronLeft /> : <FaChevronRight />;
 
-	return (
-		<div
-			onClick={clickFunction}
-			role="button"
-			tabIndex="0"
-			onKeyPress={clickFunction}
-		>
-			{icon}
-		</div>
-	);
+  return (
+    <div
+      onClick={clickFunction}
+      role="button"
+      tabIndex="0"
+      onKeyPress={clickFunction}
+    >
+      {icon}
+    </div>
+  );
 }
 
 export default function Carousel() {
-	const classes = useStyles();
-	const [index, setIndex] = useState(0);
-	const [slideIn, setSlideIn] = useState(true);
-	const [slideDirection, setSlideDirection] = useState("down");
-	const content = SLIDE_INFO[index];
-	const numSlides = SLIDE_INFO.length;
+  const classes = useStyles();
+  const [index, setIndex] = useState(0);
+  const [slideIn, setSlideIn] = useState(true);
+  const [slideDirection, setSlideDirection] = useState('down');
+  const content = SLIDE_INFO[index];
+  const numSlides = SLIDE_INFO.length;
 
-	const onArrowClick = (direction) => {
-		const increment = direction === "left" ? -1 : 1;
-		const newIndex = (index + increment + numSlides) % numSlides;
+  const onArrowClick = (direction) => {
+    const increment = direction === 'left' ? -1 : 1;
+    const newIndex = (index + increment + numSlides) % numSlides;
 
-		const oppDirection = direction === "left" ? "right" : "left";
-		setSlideDirection(direction);
-		setSlideIn(false);
+    const oppDirection = direction === 'left' ? 'right' : 'left';
+    setSlideDirection(direction);
+    setSlideIn(false);
 
-		setTimeout(() => {
-			setIndex(newIndex);
-			setSlideDirection(oppDirection);
-			setSlideIn(true);
-		}, 300);
-	};
+    setTimeout(() => {
+      setIndex(newIndex);
+      setSlideDirection(oppDirection);
+      setSlideIn(true);
+    }, 300);
+  };
 
-	// autoscroll
-	useEffect(() => {
-		const interval = setInterval(() => {
-			document.querySelector("#rightArrow").firstChild.click();
-		}, 5000);
-		return () => clearInterval(interval);
-	}, []);
+  // autoscroll
+  useEffect(() => {
+    const interval = setInterval(() => {
+      document.querySelector('#rightArrow').firstChild.click();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
-	return (
-		<Grid container justify="center" align="center" spacing={2}>
-			<Grid item className={classes.arrows}>
-				<Arrow direction="left" clickFunction={() => onArrowClick("left")} />
-			</Grid>
+  return (
+    <Grid container justify="center" align="center" spacing={2}>
+      <Grid item className={classes.arrows}>
+        <Arrow direction="left" clickFunction={() => onArrowClick('left')} />
+      </Grid>
 
-			<Slide in={slideIn} direction={slideDirection}>
-				<div>
-					<CarouselSlide content={content} />
-				</div>
-			</Slide>
-			<Grid item id="rightArrow" className={classes.arrows}>
-				<Arrow direction="right" clickFunction={() => onArrowClick("right")} />
-			</Grid>
-			<Grid container xs={12} align="center" className={classes.slideshowDots}>
-				{SLIDE_INFO.map((slide, idx) => (
-					<Grid
-						item
-						key={slide.id}
-						xs={12 / numSlides}
-						spacing={1}
-						className={
-							idx === index ? classes.slideshowDotActive : classes.slideshowDot
-						}
-					/>
-				))}
-			</Grid>
-		</Grid>
-	);
+      <Slide in={slideIn} direction={slideDirection}>
+        <div>
+          <CarouselSlide content={content} />
+        </div>
+      </Slide>
+      <Grid item id="rightArrow" className={classes.arrows}>
+        <Arrow direction="right" clickFunction={() => onArrowClick('right')} />
+      </Grid>
+      <Grid container xs={12} align="center" className={classes.slideshowDots}>
+        {SLIDE_INFO.map((slide, idx) => (
+          <Grid
+            item
+            key={slide.id}
+            xs={12 / numSlides}
+            spacing={1}
+            className={
+              idx === index ? classes.slideshowDotActive : classes.slideshowDot
+            }
+          />
+        ))}
+      </Grid>
+    </Grid>
+  );
 }

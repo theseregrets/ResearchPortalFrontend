960fe6a2fe8f35d4cc60c257d9e9a0a73713092b
Nitin <nk17oct@gmail.com>
2021-07-25 03:49:34 +0530

    add bottom-nav

--- src/Components/userDashboard/Dashboard.jsx
+++ src/Components/userDashboard/Dashboard.jsx
@@ -1,104 +1,22 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  useRouteMatch,
-  Link,
-} from 'react-router-dom';
-import clsx from 'clsx';
+import { BrowserRouter, Route, Switch, useRouteMatch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import SubjectIcon from '@material-ui/icons/Subject';
-import PeopleIcon from '@material-ui/icons/People';
-import LayersIcon from '@material-ui/icons/Layers';
 import MainDashboard from './MainDashboard';
 import Statement from './ResearchStatement';
 import Announcements from './Announcements';
 import Profile from './Profile';
+import SideNav from './Sidenav';
+import BottomNav from './BottomNav';
+import BottomNavFix from './BottomNavFix';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    // position: 'relative',
-    position: 'sticky',
-    top: '0',
-    height: '100vh',
-    whiteSpace: 'nowrap',
-    width: '240px',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
     flexDirection: 'column',
   },
-  fixedHeight: {
-    height: 240,
-  },
-  drawerListText: {
-    color: 'initial',
-    textDecoration: 'none',
-    '&:hover': {
-      textDecoration: 'none',
-    },
+  dash: {
+    display: 'flex',
   },
 }));
 
@@ -129,94 +47,39 @@ function useWindowDimensions() {
 
 export default function Dashboard() {
   const classes = useStyles();
+
   const { width } = useWindowDimensions();
-  const [open, setOpen] = React.useState(width > 900);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   const { path, url } = useRouteMatch();
 
   return (
     <BrowserRouter>
+      {/* For BottomNavFix */}
       <div className={classes.root}>
-        <CssBaseline />
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-          }}
-          open={open}
-        >
-          <div className={classes.toolbarIcon}>
-            <IconButton onClick={open ? handleDrawerClose : handleDrawerOpen}>
-              {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-            </IconButton>
-          </div>
-          <Divider />
+        <div className={classes.dash}>
+          <CssBaseline />
+          {width > 900 ? (
+            <SideNav url={url} width={width} />
+          ) : (
+            <BottomNav url={url} />
+          )}
 
-          <List>
-            <Link to={`${url}`} className={classes.drawerListText}>
-              <ListItem button>
-                <ListItemIcon>
-                  <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText primary="Dashboard" />
-              </ListItem>
-            </Link>
-
-            <Link
-              to={`${url}/research-statement`}
-              className={classes.drawerListText}
-            >
-              <ListItem button>
-                <ListItemIcon>
-                  <SubjectIcon />
-                </ListItemIcon>
-                <ListItemText primary="Research statement" />
-              </ListItem>
-            </Link>
-
-            <Link
-              to={`${url}/announcements`}
-              className={classes.drawerListText}
-            >
-              <ListItem button>
-                <ListItemIcon>
-                  <LayersIcon />
-                </ListItemIcon>
-                <ListItemText primary="Announcements" />
-              </ListItem>
-            </Link>
-
-            <Link to={`${url}/profile`} className={classes.drawerListText}>
-              <ListItem button>
-                <ListItemIcon>
-                  <PeopleIcon />
-                </ListItemIcon>
-                <ListItemText primary="Profile" />
-              </ListItem>
-            </Link>
-          </List>
-        </Drawer>
-
-        <Switch>
-          <Route exact path={path}>
-            <MainDashboard />
-          </Route>
-          <Route path={`${path}/research-statement`}>
-            <Statement />
-          </Route>
-          <Route path={`${path}/announcements`}>
-            <Announcements />
-          </Route>
-          <Route path={`${path}/profile`}>
-            <Profile />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route exact path={path}>
+              <MainDashboard />
+            </Route>
+            <Route path={`${path}/research-statement`}>
+              <Statement />
+            </Route>
+            <Route path={`${path}/announcements`}>
+              <Announcements />
+            </Route>
+            <Route path={`${path}/profile`}>
+              <Profile />
+            </Route>
+          </Switch>
+        </div>
+        {width < 900 ? <BottomNavFix /> : null}
       </div>
     </BrowserRouter>
   );

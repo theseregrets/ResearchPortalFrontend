dbe72f659391562263697baa9d2791ffca2009d7
Nitin <nk17oct@gmail.com>
2021-07-29 19:56:31 +0530

    add theme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './Components/theme/Theme';
 import Navbar from './Components/navbar/Navbar';
 import Footer from './Components/footer/Footer';
 import Homepage from './Components/homepage/homepage';
@@ -15,36 +17,38 @@ import UserDashboard from './Components/userDashboard/Dashboard';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route exact path="/milestones">
-            <Milestones />
-          </Route>
-          <Route exact path="/team">
-            <Team />
-          </Route>
-          <Route exact path="/about-us">
-            <About />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/projects">
-            <Projects />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/dashboard">
-            <UserDashboard />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/milestones">
+              <Milestones />
+            </Route>
+            <Route exact path="/team">
+              <Team />
+            </Route>
+            <Route exact path="/about-us">
+              <About />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/projects">
+              <Projects />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/dashboard">
+              <UserDashboard />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }

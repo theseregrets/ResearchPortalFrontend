6f28cc085b073f5237eb92980a35db95f20c613f
VIPIN24BOHRA <vipinbohra321@gmail.com>
2021-08-04 00:28:29 +0530

    added the branch.js in DATA folder

---
+++ src/Data/branch.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+export const Branches = [
+  {
+    value: 'CS',
+    label: 'Computer Science and Engineering',
+  },
+  {
+    value: 'EE',
+    label: 'Electrical Engineering',
+  },
+  {
+    value: 'BT',
+    label: 'Biotechnology',
+  },
+  {
+    value: 'CE',
+    label: 'Civil Engineerign',
+  },
+];
